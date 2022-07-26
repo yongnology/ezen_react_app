1c313e97881fd04e257f31166b98fc1fbfa38b30
yongnology <cassius20@naver.com>
2022-07-26 14:26:23 +0900

    2022.07.26.(화) 영역 바꾸기 방법(2)

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,14 +8,15 @@ class TOC extends Component {
         var i=0;
         while(i < data.length) {
             lists.push(<li key={data[i].id}><a
-            data-iddd={data[i].id} // i와 a태그를 구분하기 위해 data[i].id 값을 갖는 변수를 생성 
+            // data-iddd={data[i].id} // i와 a태그를 구분하기 위해 data[i].id 값을 갖는 변수를 생성 
             // 아래 dataset에 의해 연결될 예정 ↓*4
-            onClick={function(event) {
+            onClick={function(id, event) {
                 // debugger;
                 event.preventDefault();
-                this.props.onChangePage(event.target.dataset.iddd);
+                this.props.onChangePage(id);
+                // this.props.onChangePage(event.target.dataset.iddd);
                     // dataset은 data- 로 시작하는 접두사 객체 속성을 접근
-            }.bind(this)}
+            }.bind(this, data[i].id)}
             href={"/content/" + data[i].id}>{data[i].title}</a></li>);
             i++;
         }
